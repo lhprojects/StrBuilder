d90133df22a7a8a42c8ba73f5dffd2175868a671
lihawl <lihawl@126.com>
2017-06-22 22:17:48 +0800

    u

--- StrBuilder.h
+++ StrBuilder.h
@@ -1,4 +1,5 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdlib.h>
 #include <memory.h>
@@ -8,7 +9,209 @@
 #include <string>
 #include <exception>
 #include <stddef.h>
+#include <type_traits>
 
+
+namespace details {
+
+	template<class T>
+	bool is_char(T const &) {
+		return false;
+	}
+
+	template<>
+	bool is_char(char const &) {
+		return true;
+	}
+	template<>
+	bool is_char(signed char const &) {
+		return true;
+	}
+	template<>
+	bool is_char(unsigned char const &) {
+		return true;
+	}
+
+	template<class T>
+	bool is_pointer(T const &) {
+		return false;
+	}
+
+	template<class T>
+	bool is_pointer(T const * const &) {
+		return true;
+	}
+
+	template<class T>
+	bool is_pointer(T * const &) {
+		return true;
+	}
+
+	template<class T, size_t n>
+	bool is_pointer(T const (&)[n]) {
+		return true;
+	}
+
+	template<class T, size_t n>
+	bool is_pointer(T (&)[n]) {
+		return true;
+	}
+
+	template<class T>
+	bool is_string(T const &) {
+		return false;
+	}
+
+	template<>
+	bool is_string(std::string const &str) {
+		return true;
+	}
+
+	template<>
+	bool is_string(char const * const &str) {
+		return true;
+	}
+
+	template<size_t n>
+	bool is_string(char const (&str)[n]) {
+		return true;
+	}
+
+	template<class T>
+	bool is_signed(T const &) {
+		return std::is_signed<T>::value;
+	}
+
+	template<class T>
+	bool is_unsigned(T const &) {
+		return std::is_unsigned<T>::value;
+	}
+
+	template<class T>
+	bool is_integral(T const &) {
+		return std::is_integral<T>::value;
+	}
+
+	template<class T>
+	bool is_floating_point(T const &) {
+		return std::is_floating_point<T>::value;
+	}
+
+
+	struct FmtBuf {
+
+		void init(char const *fmt, size_t n) {
+			if (n <= 20) {
+				fBuf = fStaticBuf;
+			} else {
+				fBuf = new char[n + 1];
+			}
+			memcpy(fBuf, fmt, n);
+			fBuf[n] = '\0';
+		}
+
+		FmtBuf(char const *fmt, size_t n, char, char spec) {
+			init(fmt, n);
+			fBuf[n++] = spec;
+			fBuf[n++] = '\0';
+		}
+
+		FmtBuf(char const *fmt, size_t n, int, char spec) {
+			init(fmt, n);
+			fBuf[n++] = spec;
+			fBuf[n++] = '\0';
+		}
+
+		FmtBuf(char const *fmt, size_t n, unsigned, char spec) {
+			init(fmt, n);
+			fBuf[n++] = spec;
+			fBuf[n++] = '\0';
+		}
+
+		FmtBuf(char const *fmt, size_t n, long, char spec) {
+			init(fmt, n);
+			strcat(fBuf + n, "l");
+			n++;
+			fBuf[n++] = spec;
+			fBuf[n++] = '\0';
+		}
+
+		FmtBuf(char const *fmt, size_t n, unsigned long, char spec) {
+			init(fmt, n);
+			strcat(fBuf + n, "l");
+			n++;
+			fBuf[n++] = spec;
+			fBuf[n++] = '\0';
+		}
+
+		FmtBuf(char const *fmt, size_t n, long long, char spec) {
+			init(fmt, n);
+			strcat(fBuf + n, "ll");
+			n += 2;
+			fBuf[n++] = spec;
+			fBuf[n++] = '\0';
+		}
+
+		FmtBuf(char const *fmt, size_t n, unsigned long long, char spec) {
+			init(fmt, n);
+			strcat(fBuf + n, "ll");
+			n += 2;
+			fBuf[n++] = spec;
+			fBuf[n++] = '\0';
+		}
+		FmtBuf(char const *fmt, size_t n, char const*, char spec) {
+			init(fmt, n);
+			fBuf[n++] = spec;
+			fBuf[n++] = '\0';
+		}
+		FmtBuf(char const *fmt, size_t n, double, char spec) {
+			init(fmt, n);
+			fBuf[n++] = spec;
+			fBuf[n++] = '\0';
+		}
+
+		FmtBuf(char const *fmt, size_t n, void **, char spec) {
+			init(fmt, n);
+			fBuf[n++] = spec;
+			fBuf[n++] = '\0';
+		}
+
+		~FmtBuf() {
+			if (fBuf != fStaticBuf) {
+				delete fBuf;
+			}
+		}
+		char *fBuf;
+	private:
+		char fStaticBuf[32];
+	};
+
+	struct DataBuf {
+
+
+		DataBuf() {
+			fLen = 31;
+			fBuf = fStaticBuf;
+		}
+		void Double() {
+			if (fBuf != fStaticBuf) {
+				delete fBuf;
+			}
+			fLen *= 2;
+			fBuf = new char[fLen + 1];
+		}
+		~DataBuf() {
+			if (fBuf != fStaticBuf) {
+				delete fBuf;
+			}
+		}
+		size_t fLen;
+		char *fBuf;
+	private:
+		char fStaticBuf[32];
+	};
+
+}
 
 class StrBuilder
 {
@@ -30,30 +233,30 @@ public:
 	}
 
 	~StrBuilder() {
-		if (fBuf) {
-            delete fBuf;
-            fBuf = NULL;
-        }
+		Clear();
 	}
 
 	StrBuilder(char const *str) {
-		fBuf = NULL;
+		fBuf = fStaticBuf;
 		fLen = 0;
-		fCap = 0;
+		fCap = 7;
+		fBuf[0] = '\0';
 		_Append(str, strlen(str));
 	}
 
 	StrBuilder(std::string const &str) {
-		fBuf = NULL;
+		fBuf = fStaticBuf;
 		fLen = 0;
-		fCap = 0;
+		fCap = 7;
+		fBuf[0] = '\0';
 		_Append(str.data(), str.size());
 	}
 
 	StrBuilder() {
-		fBuf = NULL;
+		fBuf = fStaticBuf;
 		fLen = 0;
-		fCap = 0;
+		fCap = 7;
+		fBuf[0] = '\0';
 	}
 
 
@@ -107,18 +310,147 @@ public:
 	}
 
 	template<class Arg0, class... Args>
-	StrBuilder &Fmt(char const * fmt, Arg0&& arg0, Args&&... args) {
-		char const *b = fmt;
-		for (size_t i = 0; fmt[i]; ++i) {
+	StrBuilder &Fmt(char const * const fmt, Arg0&& arg0, Args&&... args) {
+		char const *last = fmt;
+		for (size_t i = 0; fmt[i];) {
+
 			if (fmt[i] == '%') {
 				if (fmt[i + 1] == '%') {
-					_Append(b, fmt + i - b + 1);
-					++i;
+					_Append(fmt, fmt + i + 1 - last);
+					i += 2;
+					last = fmt + i;
 				} else {
-					_Append(b, fmt + i - b);
-					App(arg0);
-					return Fmt(fmt + i + 1, std::forward<Args>(args)...);
+					_Append(last, fmt + i - last);
+					int const i_mod = i;
+					i += 1; // read `%'
+
+					// read flags
+					if (fmt[i] == '-' ||
+						fmt[i] == '+' ||
+						fmt[i] == '#' || 
+						fmt[i] == '0') {
+						++i;
+					}
+
+					// read width
+					size_t width = 0;
+					const size_t max_size = (size_t)-1;
+					const size_t max_size_div_by_10 = max_size / 10;
+					const size_t max_size_sub_by_10 = max_size - 10;
+
+					for (; fmt[i] >= '0' && fmt[i] <= '9'; ++i) {
+						if (width > max_size_div_by_10) {
+							throw std::logic_error("too large width!");
+						} else {
+							width *= 10;
+							if (width > max_size_sub_by_10) {
+								throw std::logic_error("too large width!");
+							}
+							width += 10;
+						}
+					}
+
+					// precision
+					if (fmt[i] == '.') {
+						i += 1;
+						for (; fmt[i] >= '0' && fmt[i] <= '9'; ++i) {
+						}
+					}
+
+					// specifier
+					switch (fmt[i]) {
+					case 'd': // signed or unsigned integers
+					{
+						if (details::is_integral(arg0)) {
+							_Append0(fmt + i_mod, i - i_mod, arg0);
+						} else {
+							throw std::logic_error("arg0 is not integral!");
+						}
+						break;
+					}
+					case 'o':
+					case 'x':
+					case 'X':
+					{
+						if (details::is_integral(arg0)) {
+							_AppendInt1(fmt + i_mod, i - i_mod, arg0, fmt[i]);
+						} else {
+							throw std::logic_error("arg0 is not integral!");
+						}
+						break;
+					}
+					case 'i':
+					{
+						if (details::is_signed(arg0)) {
+							_AppendInt1(fmt + i_mod, i - i_mod, arg0, 'i');
+						} else {
+							throw std::logic_error("arg0 is not signed!");
+						}
+						break;
+					}
+					case 'u': // unsigned integers
+					{
+						if (details::is_unsigned(arg0)) {
+							_AppendInt1(fmt + i_mod, i - i_mod, arg0, 'u');
+						} else {
+							throw std::logic_error("arg0 is not unsigned!");
+						}
+						break;
+					}
+					case 'f': // float or double
+					case 'F': // float or double
+					case 'e': // float or double
+					case 'E': // float or double
+					case 'g': // float or double
+					case 'G': // float or double
+					case 'a': // float or double
+					case 'A': // float or double
+					{
+						if (details::is_floating_point(arg0)) {
+							_AppendInt1(fmt + i_mod, i - i_mod, arg0, fmt[i]);
+						} else {
+							throw std::logic_error("arg0 is not floating point!");
+						}
+						break;
+					}
+					case 'c': // char
+					{
+						if (details::is_char(arg0)) {
+							_AppendInt1(fmt + i_mod, i - i_mod, arg0, 'c');
+						} else {
+							throw std::logic_error("arg0 is not floating point!");
+						}
+						break;
+					}
+					case 's': // std::string or char const*
+					{
+						if (details::is_string(arg0)) {
+							_Append0(fmt + i_mod, i - i_mod, arg0);
+						} else {
+							throw std::logic_error("arg0 is not string!");
+						}
+						break;
+					}
+					case 'p': // pointer
+					{
+						if (details::is_pointer(arg0)) {
+							_AppendPointer(fmt + i_mod, i - i_mod, arg0, 'p');
+						} else {
+							throw std::logic_error("arg0 is not pointer!");
+						}
+						break;
+					}
+					case 'n': // any type
+						break;
+					default:
+						break;
+					}
+					i += 1;
+
+					return Fmt(fmt + i, std::forward<Args>(args)...);
 				}
+			} else {
+				i += 1;
 			}
 		}
 		throw std::logic_error("there are unused parameter(s)");
@@ -143,19 +475,139 @@ public:
 		return *this;
 	}
 
+	void Clear() {
+		if (fBuf != fStaticBuf) {
+			delete fBuf;
+		}
+		fBuf = fStaticBuf;
+		fLen = 0;
+		fCap = 7;
+	}
 
 private:
+
+	template<class Int>
+	void _Append0(char const *fmt_, size_t n, Int const &arg) {
+	}
+	void _Append0(char const *fmt_, size_t n, signed char arg) {
+		_AppendArich(fmt_, n, arg, 'i');
+	}
+	void _Append0(char const *fmt_, size_t n, short arg) {
+		_AppendArich(fmt_, n, arg, 'i');
+	}
+	void _Append0(char const *fmt_, size_t n, int arg) {
+		_AppendArich(fmt_, n, arg, 'i');
+	}
+	void _Append0(char const *fmt_, size_t n, long arg) {
+		_AppendArich(fmt_, n, arg, 'i');
+	}
+	void _Append0(char const *fmt_, size_t n, long long arg) {
+		_AppendArich(fmt_, n, arg, 'i');
+	}
+	void _Append0(char const *fmt_, size_t n, unsigned char arg) {
+		_AppendArich(fmt_, n, arg, 'u');
+	}
+	void _Append0(char const *fmt_, size_t n, unsigned short arg) {
+		_AppendArich(fmt_, n, arg, 'u');
+	}
+	void _Append0(char const *fmt_, size_t n, unsigned int arg) {
+		_AppendArich(fmt_, n, arg, 'u');
+	}
+	void _Append0(char const *fmt_, size_t n, unsigned long arg) {
+		_AppendArich(fmt_, n, arg, 'u');
+	}
+	void _Append0(char const *fmt_, size_t n, unsigned long long arg) {
+		_AppendArich(fmt_, n, arg, 'u');
+	}
+	void _Append0(char const *fmt_, size_t n, std::string const &arg) {
+		_AppendArich(fmt_, n, arg.c_str(), 's');
+	}
+	void _Append0(char const *fmt_, size_t n, char const *arg) {
+		_AppendArich(fmt_, n, arg, 's');
+	}
+
+	template<class Int>
+	void _AppendInt1(char const *fmt_, size_t n, Int const &arg, char spec) {
+	}
+
+	void _AppendInt1(char const *fmt_, size_t n, signed char arg, char spec) {
+		_AppendArich(fmt_, n, arg, spec);
+	}
+	void _AppendInt1(char const *fmt_, size_t n, short arg, char spec) {
+		_AppendArich(fmt_, n, arg, spec);
+	}
+	void _AppendInt1(char const *fmt_, size_t n, int arg, char spec) {
+		_AppendArich(fmt_, n, arg, spec);
+	}
+	void _AppendInt1(char const *fmt_, size_t n, long arg, char spec) {
+		_AppendArich(fmt_, n, arg, spec);
+	}
+	void _AppendInt1(char const *fmt_, size_t n, long long arg, char spec) {
+		_AppendArich(fmt_, n, arg, spec);
+	}
+	void _AppendInt1(char const *fmt_, size_t n, unsigned char arg, char spec) {
+		_AppendArich(fmt_, n, arg, spec);
+	}
+	void _AppendInt1(char const *fmt_, size_t n, unsigned short arg, char spec) {
+		_AppendArich(fmt_, n, arg, spec);
+	}
+	void _AppendInt1(char const *fmt_, size_t n, unsigned int arg, char spec) {
+		_AppendArich(fmt_, n, arg, spec);
+	}
+	void _AppendInt1(char const *fmt_, size_t n, unsigned long arg, char spec) {
+		_AppendArich(fmt_, n, arg, spec);
+	}
+	void _AppendInt1(char const *fmt_, size_t n, unsigned long long arg, char spec) {
+		_AppendArich(fmt_, n, arg, spec);
+	}
+	void _AppendInt1(char const *fmt_, size_t n, double arg, char spec) {
+		_AppendArich(fmt_, n, arg, spec);
+	}
+	void _AppendInt1(char const *fmt_, size_t n, float arg, char spec) {
+		_AppendArich(fmt_, n, arg, spec);
+	}
+
+	template<class T>
+	void _AppendPointer(char const *fmt_, size_t n, T const &arg, char spec) {
+	}
+	void _AppendPointer(char const *fmt_, size_t n, void const *arg, char spec) {
+		_AppendArich(fmt_, n, (void**)arg, spec);
+	}
+	void _AppendPointer(char const *fmt_, size_t n, void *arg, char spec) {
+		_AppendArich(fmt_, n, (void**)arg, spec);
+	}
+
+	template<class Int>
+	void _AppendArich(char const *fmt_, size_t n, Int arg, char spec) {
+
+		details::FmtBuf fmt(fmt_, n, arg, spec);
+		details::DataBuf data;
+		int n_write = 0;
+		for (;; ) {
+
+			//Number of characters that would have been written for a sufficiently
+			// large buffer if successful(not including the terminating null character),
+			//or a negative value if an error occurred.Thus, the(null - terminated) 
+			// output has been completely written if and only if the returned value
+			// is nonnegative and less than buf_size
+			n_write = snprintf(data.fBuf, data.fLen, fmt.fBuf, arg);
+			if (n_write >= 0) {
+				break;
+			} else {
+				data.Double();
+			}
+		}
+		_Append(data.fBuf, n_write);
+	}
+
 	void _Append(char const *str, size_t len) {
 		if (len != 0) {
 			if (fLen + len >= fCap) {
-				if (fCap == 0) fCap = 20;
-				for (; fCap <= fLen + len; fCap += 1, fCap *= 3, fCap /= 2) {
+				for (; fCap <= fLen + len; fCap *= 3, fCap /= 2) {
 				}
 				char *b = new char[fCap];
-				if (fBuf) {
-					memcpy(b, fBuf, fLen);
-					delete fBuf;
-				}
+				memcpy(b, fBuf, fLen);
+				if (fBuf != fStaticBuf) delete fBuf;
 				fBuf = b;
 			}
 			assert(fBuf);
@@ -168,6 +620,7 @@ private:
 	char *fBuf;
 	size_t fLen;
 	size_t fCap;
+	char fStaticBuf[8];
 };
 
 template<class... Args>
--- StrBuilderExamples.cpp
+++ StrBuilderExamples.cpp
@@ -1,14 +1,116 @@
 
 #include "StrBuilder.h"
 
-
+#define ASSERT(x, y) if(strcmp(x, y) != 0) { printf("failed: %s != %s\n", x, y); }
 int main() {
+	char b[1024];
 	StrBuilder sb;
-	sb.Fmt("% %% %", 1, "abc");
-	sb.App("abc");
-	sb.App(1, "abc");
-	sb.App((size_t)1, "abc");
 
-	sb.Clone();
+
+	sb.Fmt("%3d", 1);
+	sprintf(b, "%3d", 1);
+	ASSERT(sb.Data(), b);
+	sb.Clear();
+
+	sb.Fmt("%3d", (unsigned long long)-1);
+	sprintf(b, "%3llu", (unsigned long long)-1);
+	ASSERT(sb.Data(), b);
+	sb.Clear();
+
+	sb.Fmt("%3d", (signed long long) - 1);
+	sprintf(b, "%3lli", (signed long long) - 1);
+	ASSERT(sb.Data(), b);
+	sb.Clear();
+
+	sb.Fmt("%3d", (unsigned long) - 1);
+	sprintf(b, "%3lu", (unsigned long) - 1);
+	ASSERT(sb.Data(), b);
+	sb.Clear();
+
+	sb.Fmt("%3d", (signed long) - 1);
+	sprintf(b, "%3li", (signed long) - 1);
+	ASSERT(sb.Data(), b);
+	sb.Clear();
+
+	sb.Fmt("%3d", (unsigned short) - 1);
+	sprintf(b, "%3u", (unsigned short) - 1);
+	ASSERT(sb.Data(), b);
+	sb.Clear();
+
+	sb.Fmt("%3d", (signed short)-1);
+	sprintf(b, "%3d", (signed short)-1);
+	ASSERT(sb.Data(), b);
+	sb.Clear();
+
+	sb.Fmt("%3d", (unsigned char)-1);
+	sprintf(b, "%3u", (unsigned char)-1);
+	ASSERT(sb.Data(), b);
+	sb.Clear();
+
+	sb.Fmt("%3d", (signed char)-1);
+	sprintf(b, "%3d", (signed char)-1);
+	ASSERT(sb.Data(), b);
+	sb.Clear();
+
+	sb.Fmt("%c", (signed char)'a');
+	sprintf(b, "%c", (signed char)'a');
+	ASSERT(sb.Data(), b);
+	sb.Clear();
+
+	sb.Fmt("%c", (unsigned char)'a');
+	sprintf(b, "%c", (signed char)'a');
+	ASSERT(sb.Data(), b);
+	sb.Clear();
+
+	sb.Fmt("%c", (unsigned char)'a');
+	sprintf(b, "%c", (unsigned char)'a');
+	ASSERT(sb.Data(), b);
+	sb.Clear();
+
+
+	sb.Fmt("%3.3f", 1.1);
+	sprintf(b, "%3.3f", 1.1);
+	ASSERT(sb.Data(), b);
+	sb.Clear();
+
+	sb.Fmt("%3s", "abc");
+	sprintf(b, "%3s", "abc");
+	ASSERT(sb.Data(), b);
+	sb.Clear();
+
+	sb.Fmt("%6s", std::string("abc"));
+	sprintf(b, "%6s", "abc");
+	ASSERT(sb.Data(), b);
+	sb.Clear();
+
+	void * a_pointer = &sb;
+	sb.Fmt("%6p", a_pointer);
+	sprintf(b, "%6p", a_pointer);
+	ASSERT(sb.Data(), b);
+	sb.Clear();
+
+	try {
+		sb.Fmt("%c", 1);
+		printf("faield %s", R"--(	sb.Fmt("%c", 1)		)--");
+	}
+	catch (...) {
+	}
+	sb.Clear();
+
+	try {
+		sb.Fmt("%u", (int)1);
+		printf("faield %s", R"--(	sb.Fmt("%u", (long long)1)		)--");
+	}
+	catch (...) {
+	}
+	sb.Clear();
+
+	try {
+		sb.Fmt("%i", (unsigned)1);
+		printf("faield %s", R"--(	sb.Fmt("%i", (unsigned)1)		)--");
+	}
+	catch (...) {
+	}
+	sb.Clear();
 
 }
